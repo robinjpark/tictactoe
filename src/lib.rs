5aacc96815b5d4d2249dfa33be0e9182809131e1
Robin Park <robin.j.park@gmail.com>
2020-07-30 09:37:39 -0400

    Add Board structure to represent the game board
    - Add a Board structure, containing a grid of positions, which may
      optionally contain an X or an O.
    - Add a constructor which creates an empty board
    - Add corresponding unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,20 @@ enum Player {
     O,
 }
 
+#[allow(dead_code)]
+struct Board {
+    positions: [[Option<Player>; 3]; 3],
+}
+
+impl Board {
+    #[allow(dead_code)]
+    fn new() -> Board {
+        Board { positions: [[None, None, None],
+                            [None, None, None],
+                            [None, None, None]]}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,5 +30,15 @@ mod tests {
         let player3 = Player::X;
         assert_ne!(player1, player2);
         assert_eq!(player1, player3);
+    }
+
+    #[test]
+    fn test_empty_board() {
+        let empty = Board::new();
+        for row in 0..2 {
+            for column in 0..2 {
+                assert_eq!(None, empty.positions[row][column]);
+            }
+        }
     }
 }
