eef84e1516b164c0461c2c21bd6b0fcd66b29186
Robin Park <robin.j.park@gmail.com>
2020-08-03 13:05:22 -0400

    Derive Default in Board to silence clippy

--- src/board.rs
+++ src/board.rs
@@ -58,7 +58,7 @@ impl Position {
     }
 }
 
-#[derive(PartialEq, Debug, Copy, Clone)]
+#[derive(Default, PartialEq, Debug, Copy, Clone)]
 /// Represents a tic-tac-toe game board.
 pub struct Board {
     #[doc(hidden)]
